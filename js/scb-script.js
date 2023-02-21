10425bcd72c80a8744eff28436f5d14d17ca281c
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-21 15:37:59 +0100

    FEAT: stampato con v-for la lista a schermo

--- js/scb-script.js
+++ js/scb-script.js
@@ -1,4 +1,52 @@
 console.log("Ciao Cianuro... Oggi l'esercizio è VUE TO DO LIST");
+
+//********************
+// CREO ARRAY DI OGGETTI CON LE COSE DA FARE
+//******************** */
+let toDo = [
+  {
+    text: "Pubblicare e programmare su FB gli orari di Aprile",
+    done: false,
+  },
+  {
+    text: "Impaginare il Calendario 2024",
+    done: false,
+  },
+  {
+    text: "Impaginare il Bollettino di Pasqua",
+    done: false,
+  },
+  {
+    text: "Ritirare il quadro per la sala da pranzo",
+    done: false,
+  },
+  {
+    text: "Portare la macchina dal meccanico per il tagliando annuale",
+    done: false,
+  },
+  {
+    text: "Montare l'antenna Booster LTE",
+    done: false,
+  },
+  {
+    text: "Pagare Bollo auto",
+    done: false,
+  },
+  {
+    text: "Ritirare e pagare l'Assicurazione",
+    done: false,
+  },
+  {
+    text: "Rinnovare abbonamento Amazon Prime",
+    done: false,
+  },
+  {
+    text: "Aggiornare il mac e gli iDevice all'ultima relaise del SO",
+    done: false,
+  },
+];
+console.log(toDo);
+
 //---------------------------------------------------------------------------
 // ||||||||||||||||||||||||||| VUE ||||||||||||||||||||||||||||
 //---------------------------------------------------------------------------
@@ -7,7 +55,8 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      message: "cianuro's to do list",
+      toDo: toDo,
     };
   },
 }).mount("#app");
